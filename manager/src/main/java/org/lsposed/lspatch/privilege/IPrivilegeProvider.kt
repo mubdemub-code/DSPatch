4bff548fc30091e954c6723a1e318240cdf24d9b
mubdemub-code <mubdemub@gmail.com>
2026-03-12 22:14:23 +0100

    Update IPrivilegeProvider.kt

--- manager/src/main/java/org/lsposed/lspatch/privilege/IPrivilegeProvider.kt
+++ manager/src/main/java/org/lsposed/lspatch/privilege/IPrivilegeProvider.kt
@@ -1,8 +1,5 @@
 package org.lsposed.lspatch.privilege
 
-/**
- * C'est le contrat : tout moteur de privilège doit respecter ces règles.
- */
 interface IPrivilegeProvider {
     fun isAvailable(): Boolean
     fun getName(): String

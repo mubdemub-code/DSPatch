db4946a3566041ceb34fb4e3a8de6ffd41185c55
mubdemub-code <mubdemub@gmail.com>
2026-03-13 11:00:17 +0100

    Update ShizukuService.kt

--- manager/src/main/java/org/lsposed/lspatch/ShizukuService.kt
+++ manager/src/main/java/org/lsposed/lspatch/ShizukuService.kt
@@ -1,16 +1,29 @@
 package org.lsposed.lspatch
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import org.lsposed.lspatch.IShizukuService
 import kotlin.system.exitProcess
 
 class ShizukuService : IShizukuService.Stub() {
+
     override fun runShellCommand(cmd: String): String {
         return try {
-            val process = Runtime.getRuntime().exec(cmd)
-            val output = process.inputStream.bufferedReader().readText()
-            val error = process.errorStream.bufferedReader().readText()
+            val process = ProcessBuilder("sh", "-c", cmd)
+                .redirectErrorStream(true)
+                .start()
+
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            val output = StringBuilder()
+
+            reader.lines().forEach {
+                output.append(it).append("\n")
+            }
+
             process.waitFor()
-            output + error
+
+            output.toString()
+
         } catch (e: Exception) {
             e.stackTraceToString()
         }

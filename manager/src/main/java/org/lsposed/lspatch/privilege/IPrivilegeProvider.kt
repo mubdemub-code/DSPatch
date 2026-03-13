5020c5bebac4422760847d330cb8aac5976730b3
mubdemub-code <mubdemub@gmail.com>
2026-03-13 09:40:07 +0100

    Update IPrivilegeProvider.kt

--- manager/src/main/java/org/lsposed/lspatch/privilege/IPrivilegeProvider.kt
+++ manager/src/main/java/org/lsposed/lspatch/privilege/IPrivilegeProvider.kt
@@ -1,44 +1,14 @@
-// manager/src/main/java/org/lsposed/lspatch/privilege/IPrivilegeProvider.kt
 package org.lsposed.lspatch.privilege
 
 import android.content.IntentSender
 import android.content.pm.PackageInstaller
 
 interface IPrivilegeProvider {
-    /**
-     * Nom du fournisseur (Shizuku, Dhizuku, etc.)
-     */
     fun getName(): String
-
-    /**
-     * Vérifie si le service est disponible et les permissions accordées
-     */
     fun isAvailable(): Boolean
-
-    /**
-     * Crée une session d'installation d'APK
-     */
-    fun createPackageInstallerSession(
-        params: PackageInstaller.SessionParams
-    ): PackageInstaller.Session
-
-    /**
-     * Vérifie si une app est installée SANS le patch
-     */
+    fun createPackageInstallerSession(params: PackageInstaller.SessionParams): PackageInstaller.Session
     fun isPackageInstalledWithoutPatch(packageName: String): Boolean
-
-    /**
-     * Désinstalle un package
-     */
     fun uninstallPackage(packageName: String, intentSender: IntentSender)
-
-    /**
-     * Optimise un package (DEX compilation)
-     */
     suspend fun performDexOptMode(packageName: String): Boolean
-
-    /**
-     * Exécute une commande shell
-     */
     suspend fun executeShellCommand(command: String): String?
 }
